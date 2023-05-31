ae55c26547b0357ae0ea18c2f491473a59c32689
Arif Hidayatullah <arifhidayatullah961@gmail.com>
2023-05-31 12:36:32 +0700

    UTS PBO (5/04/2023)

---
+++ src/BankBRO/InformasiSaldoModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankBRO;
+import BankBRO.TransferModel;
+
+/**
+ *
+ * @author lenovo
+ */
+public class InformasiSaldoModel {
+    String saldo="9000000";
+    
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/BankBRO/PinModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankBRO;
+
+/**
+ *
+ * @author lenovo
+ */
+public class PinModel {
+   
+    String pin = "250303";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    
+    
+    
+}
